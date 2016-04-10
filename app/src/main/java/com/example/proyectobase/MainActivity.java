8bbce439eb3163e8e6a5c0dd27577b3b434ed448
Mario Velasco <m3ario@gmail.com>
2016-04-11 01:17:53 +0200

    Select camera and resolution

--- app/src/main/java/com/example/proyectobase/MainActivity.java
+++ app/src/main/java/com/example/proyectobase/MainActivity.java
@@ -1,19 +1,12 @@
 package com.example.proyectobase;
 
-import android.Manifest;
 import android.app.Activity;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.WindowManager;
 import android.widget.Toast;
 
@@ -23,13 +16,15 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 
-import java.security.Permission;
-
 public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2,
         LoaderCallbackInterface {
     private static final String TAG = "MainActivity";
+    private static final String STATE_CAMERA_INDEX = "cameraIndex";
 
     private CameraBridgeViewBase cameraView;
+    private int indiceCamara = CameraBridgeViewBase.CAMERA_ID_BACK;
+    private int cam_anchura = 320;// resolucion deseada de la imagen
+    private int cam_altura = 240;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,6 +34,22 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
         PermissionManager.checkCameraPermission(this);
         cameraView = (CameraBridgeViewBase) findViewById(R.id.vista_camara);
         cameraView.setCvCameraViewListener(this);
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save the current camera index.
+        savedInstanceState.putInt(STATE_CAMERA_INDEX, indiceCamara);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null) {
+            indiceCamara = savedInstanceState.getInt(
+                    STATE_CAMERA_INDEX, CameraBridgeViewBase.CAMERA_ID_BACK);
+        }
+        cameraView.setCameraIndex(indiceCamara);
     }
 
     @Override
@@ -64,6 +75,8 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
 //Interface CvCameraViewListener2
 
     public void onCameraViewStarted(int width, int height) {
+        cam_altura = height; //Estas son las que se usan de verdad
+        cam_anchura = width;
     }
 
     public void onCameraViewStopped() {
@@ -79,6 +92,7 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
         switch (status) {
             case LoaderCallbackInterface.SUCCESS:
                 Log.i(TAG, "OpenCV se cargo correctamente");
+                cameraView.setMaxFrameSize(cam_anchura, cam_altura);
                 cameraView.enableView();
                 break;
             default:
@@ -94,4 +108,57 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
                                  InstallCallbackInterface callback) {
     }
 
+    public boolean onTouchEvent(MotionEvent event) {
+        openOptionsMenu();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.cambiarCamara:
+                if (indiceCamara == CameraBridgeViewBase.CAMERA_ID_BACK) {
+                    indiceCamara = CameraBridgeViewBase.CAMERA_ID_FRONT;
+                } else {
+                    indiceCamara = CameraBridgeViewBase.CAMERA_ID_BACK;
+                }
+                recreate();
+                break;
+            case R.id.resolucion_800x600:
+                cam_anchura = 800;
+                cam_altura = 600;
+                reiniciarResolucion();
+                break;
+            case R.id.resolucion_640x480:
+                cam_anchura = 640;
+                cam_altura = 480;
+                reiniciarResolucion();
+                break;
+            case R.id.resolucion_320x240:
+                cam_anchura = 320;
+                cam_altura = 240;
+                reiniciarResolucion();
+                break;
+        }
+        String msg = "W=" + Integer.toString(cam_anchura) + " H= " +
+                Integer.toString(cam_altura) + " Cam= " +
+                Integer.toBinaryString(indiceCamara);
+        Toast.makeText(MainActivity.this, msg,
+                Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+    public void reiniciarResolucion() {
+        cameraView.disableView();
+        cameraView.setMaxFrameSize(cam_anchura, cam_altura);
+        cameraView.enableView();
+    }
 }
