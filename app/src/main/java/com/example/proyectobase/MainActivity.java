cfa2ded537732a5c7b2a58c19cb6e0ba22b288de
Mario Velasco <m3ario@gmail.com>
2016-04-17 20:03:14 +0200

    Select input type and save image (Base 4)

--- app/src/main/java/com/example/proyectobase/MainActivity.java
+++ app/src/main/java/com/example/proyectobase/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.proyectobase;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,12 +17,22 @@ import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.InstallCallbackInterface;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
 
 public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2,
         LoaderCallbackInterface {
     private static final String TAG = "MainActivity";
     private static final String STATE_CAMERA_INDEX = "cameraIndex";
+    private int tipoEntrada = 0;  // 0 -> cámara  1 -> fichero1  2 -> fichero2
+    Mat imagenRecurso_;
+    boolean recargarRecurso = false;
+    private boolean guardarSiguienteImagen = false;
 
     private CameraBridgeViewBase cameraView;
     private int indiceCamara = CameraBridgeViewBase.CAMERA_ID_BACK;
@@ -83,7 +96,70 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
     }
 
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        return inputFrame.rgba();
+        Mat entrada;
+        if (tipoEntrada == 0) {
+            entrada = inputFrame.rgba();
+        } else {
+            if (recargarRecurso == true) {
+                imagenRecurso_ = new Mat();
+//Poner aqui el nombre de los archivos copiados
+                int RECURSOS_FICHEROS[] = {0, R.raw.img1, R.raw.img2};
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+                        RECURSOS_FICHEROS[tipoEntrada]);
+//Convierte el recurso a una Mat de OpenCV
+                Utils.bitmapToMat(bitmap, imagenRecurso_);
+                recargarRecurso = false;
+            }
+            entrada = imagenRecurso_;
+        }
+        Mat salida = entrada.clone();
+
+        if (guardarSiguienteImagen) {//Para foto salida debe ser rgba
+            takePhoto(entrada, salida);
+            guardarSiguienteImagen = false;
+        }
+        if(tipoEntrada > 0) {
+        //Es necesario que el tamaño de la salida coincida con el real de la captura de la camara
+            Imgproc.resize(salida, salida, new Size(cam_anchura , cam_altura));
+        }
+        return salida;
+    }
+
+    private void takePhoto(final Mat input, final Mat output) {
+        // Determina la ruta para crear los archivos
+        final long currentTimeMillis = System.currentTimeMillis();
+        final String appName = getString(R.string.app_name);
+        final String galleryPath = Environment
+                .getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_PICTURES).toString();
+        final String albumPath = galleryPath + "/" + appName;
+        final String photoPathIn = albumPath + "/In_" + currentTimeMillis
+                + ".png";
+        final String photoPathOut = albumPath + "/Out_" + currentTimeMillis
+                + ".png";
+        // Asegurarse que el directorio existe
+        File album = new File(albumPath);
+        if (!album.isDirectory() && !album.mkdirs()) {
+            Log.e(TAG, "Error al crear el directorio " + albumPath);
+            return;
+        }
+        // Intenta crear los archivos
+        Mat mBgr = new Mat();
+        if (output.channels() == 1)
+            Imgproc.cvtColor(output, mBgr, Imgproc.COLOR_GRAY2BGR, 3);
+        else
+            Imgproc.cvtColor(output, mBgr, Imgproc.COLOR_RGBA2BGR, 3);
+        if (!Imgcodecs.imwrite(photoPathOut, mBgr)) {
+            Log.e(TAG, "Fallo al guardar " + photoPathOut);
+        }
+        if (input.channels() == 1)
+            Imgproc.cvtColor(input, mBgr, Imgproc.COLOR_GRAY2BGR, 3);
+        else
+            Imgproc.cvtColor(input, mBgr, Imgproc.COLOR_RGBA2BGR, 3);
+        if (!Imgcodecs.imwrite(photoPathIn, mBgr))
+            Log.e(TAG, "Fallo al guardar " + photoPathIn);
+        mBgr.release();
+        return;
     }
 
 //Interface LoaderCallbackInterface
@@ -146,6 +222,20 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
                 cam_anchura = 320;
                 cam_altura = 240;
                 reiniciarResolucion();
+                break;
+            case R.id.entrada_camara:
+                tipoEntrada = 0;
+                break;
+            case R.id.entrada_fichero1:
+                tipoEntrada = 1;
+                recargarRecurso = true;
+                break;
+            case R.id.entrada_fichero2:
+                tipoEntrada = 2;
+                recargarRecurso = true;
+                break;
+            case R.id.guardar_imagenes:
+                guardarSiguienteImagen = true;
                 break;
         }
         String msg = "W=" + Integer.toString(cam_anchura) + " H= " +
