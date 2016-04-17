014bc5ec776108cbf7e38764a0d4e59f500390cf
Mario Velasco <m3ario@gmail.com>
2016-04-17 20:58:26 +0200

    select red color

--- app/src/main/java/com/example/proyectobase/Procesador.java
+++ app/src/main/java/com/example/proyectobase/Procesador.java
@@ -1,6 +1,8 @@
 package com.example.proyectobase;
 
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 /**
  * Author: Mario Velasco Casquero
@@ -8,13 +10,28 @@ import org.opencv.core.Mat;
  * Email: m3ario@gmail.com
  */
 public class Procesador {
+    Mat red;
+    Mat green;
+    Mat blue;
+    Mat maxGB;
 
     public Procesador() { //Constructor
-
+        red = new Mat();
+        green = new Mat();
+        blue = new Mat();
+        maxGB = new Mat();
     }
 
     public Mat procesa(Mat entrada) {
-        Mat salida = entrada.clone();
+        Mat salida = new Mat();
+        Core.extractChannel(entrada, red, 0);
+        Core.extractChannel(entrada, green, 1);
+        Core.extractChannel(entrada, blue, 2);
+
+        Core.max(green, blue, maxGB);
+        Core.subtract(red, maxGB, salida);
+        if(salida.channels() == 1)
+            Imgproc.cvtColor(salida, salida, Imgproc.COLOR_GRAY2RGBA);
         return salida;
     }
 }
