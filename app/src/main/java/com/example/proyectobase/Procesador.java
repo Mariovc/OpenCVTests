0de06f4d2130b8cd71ef2ea44f2a201c77f2a114
Mario Velasco <m3ario@gmail.com>
2016-04-17 21:22:00 +0200

    High-pass filter

--- app/src/main/java/com/example/proyectobase/Procesador.java
+++ app/src/main/java/com/example/proyectobase/Procesador.java
@@ -1,6 +1,10 @@
 package com.example.proyectobase;
 
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 
 /**
  * Author: Mario Velasco Casquero
@@ -8,13 +12,25 @@ import org.opencv.core.Mat;
  * Email: m3ario@gmail.com
  */
 public class Procesador {
-
-    public Procesador() { //Constructor
-
+    Mat paso_bajo;
+    public Procesador() {
+        paso_bajo= new Mat();
     }
 
     public Mat procesa(Mat entrada) {
-        Mat salida = entrada.clone();
+        Mat salida = new Mat();
+        int filter_size = 17;
+        Size s=new Size(filter_size,filter_size);
+
+        Imgproc.blur(entrada, paso_bajo, s);
+
+        // Hacer la resta. Los valores negativos saturan a cero
+        Core.subtract(paso_bajo, entrada, salida);
+
+        //Aplicar Ganancia para ver mejor. La multiplicacion satura
+        Scalar ganancia = new Scalar(2);
+        Core.multiply(salida, ganancia, salida);
+
         return salida;
     }
 }
