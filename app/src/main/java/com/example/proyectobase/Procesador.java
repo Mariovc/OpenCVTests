b4d9a693d7ea14c5f6646b2c8c119da6d0506a80
Mario Velasco <m3ario@gmail.com>
2016-04-17 22:29:34 +0200

    Edge detection over red image

--- app/src/main/java/com/example/proyectobase/Procesador.java
+++ app/src/main/java/com/example/proyectobase/Procesador.java
@@ -18,6 +18,7 @@ public class Procesador {
     Mat ModGrad2;
     Mat ModGrad;
     Mat ModAng;
+    Mat red, green, blue, maxGB;
 
     public Procesador() { //Constructor
         Gx = new Mat();
@@ -27,18 +28,32 @@ public class Procesador {
         ModGrad = new Mat();
         ModGrad2 = new Mat();
         ModAng = new Mat();
+        red = new Mat();
+        green = new Mat();
+        blue = new Mat();
+        maxGB = new Mat();
     }
 
     public Mat procesa(Mat entrada) {
         Mat salida = new Mat();
-        Imgproc.Sobel(entrada, Gx, CvType.CV_32FC1 , 1, 0); //Derivada primera rto x
-        Imgproc.Sobel(entrada, Gy, CvType.CV_32FC1, 0, 1); //Derivada primera rto y
+        Mat entradaRoja = new Mat();
+        Core.extractChannel(entrada, red, 0);
+        Core.extractChannel(entrada, green, 1);
+        Core.extractChannel(entrada, blue, 2);
+
+        Core.max(green, blue, maxGB);
+        Core.subtract(red, maxGB, entradaRoja);
+        //if(entradaRoja.channels() == 1)
+        //    Imgproc.cvtColor(entradaRoja, entradaRoja, Imgproc.COLOR_GRAY2RGBA);
+
+        Imgproc.Sobel(entradaRoja, Gx, CvType.CV_32FC1 , 1, 0); //Derivada primera rto x
+        Imgproc.Sobel(entradaRoja, Gy, CvType.CV_32FC1, 0, 1); //Derivada primera rto y
         Core.multiply(Gx, Gx, Gx2); //Gx2 = Gx*Gx elemento a elemento
         Core.multiply(Gy, Gy , Gy2); //Gy2 = Gy*Gy elemento a elemento
         ModGrad = new Mat();
-        Core.add( Gx2 , Gy2, ModGrad2);
+        Core.add(Gx2, Gy2, ModGrad2);
         ModGrad = new Mat();
-        Core.sqrt(ModGrad2,ModGrad);
+        Core.sqrt(ModGrad2, ModGrad);
         Core.cartToPolar(Gx, Gy, ModGrad, ModAng);
         ModGrad.convertTo(salida, CvType.CV_8UC1);
         return salida;
